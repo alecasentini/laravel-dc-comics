6dc9a68041a14e71391184821c29879fb302bd43
alecasentini <ale.casentini@gmail.com>
2023-06-14 17:15:46 +0200

    Creazione e migrazione Seed (no artists e writers)

--- database/migrations/2023_06_14_131243_create_comics_table.php
+++ database/migrations/2023_06_14_131243_create_comics_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('thumb', 500)->nullable();
+            $table->string('price')->nullable();
+            $table->string('series')->nullable();
+            $table->date('sale_date')->nullable();
+            $table->string('type')->nullable();
+            $table->string('artists')->nullable();
+            $table->string('writers')->nullable();
             $table->timestamps();
         });
     }
